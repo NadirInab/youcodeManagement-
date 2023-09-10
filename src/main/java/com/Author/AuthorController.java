0bd146683f228afd94ef0523cf814c04ad40f645
nadir inab <nadir.inab.dev@gmail.com>
2023-09-11 00:03:24 +0100

    Author controller is added

--- src/main/java/com/Author/AuthorController.java
+++ src/main/java/com/Author/AuthorController.java
@@ -1,23 +1,24 @@
 package main.java.com.Author;
 
+
 import main.java.com.DB.DatabaseConnection;
 import main.java.com.Book.Book ;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import main.java.com.Author.AuthorService ;
 
 public class AuthorController {
 
-        public void save(Book book) {
-            try (Connection connection = DatabaseConnection.getConnection()) {
-                String sql = "INSERT INTO books (title, author_id) VALUES (?, ?)";
-                PreparedStatement statement = connection.prepareStatement(sql);
-                statement.setString(1, book.getTitle());
-                statement.setInt(2, book.getAuthor().getId());
-                statement.executeUpdate();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+    Connection connection ;
+
+    AuthorService authorService = new AuthorService() ;
+    public AuthorController() throws Exception{
+        DatabaseConnection databaseConnection = new DatabaseConnection() ;
+        this.connection = DatabaseConnection.getConnection() ;
     }
+
+    public void add(Author author){
+        this.authorService.addAuthor(author) ;
+    }
+}
