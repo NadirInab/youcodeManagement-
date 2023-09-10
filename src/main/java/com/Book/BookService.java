9616c0ff60f478d2bad991e9ba42e084f31f8ab8
nadir inab <nadir.inab.dev@gmail.com>
2023-09-11 00:00:11 +0100

    delete book method is added

--- src/main/java/com/Book/BookService.java
+++ src/main/java/com/Book/BookService.java
@@ -12,6 +12,44 @@ public class BookService {
         this.connection = DatabaseConnection.getConnection();
     }
 
+    public void getBooks(){
+        if (this.connection != null) {
+            String query = "SELECT * FROM book";
+            System.out.println("Books Details : ");
+            System.out.format("%s\t%s\t%s\t%s\t%s\n", "Id", "Title", "Author", "Isbn","Status");
+            try {
+                Statement stmt = connection.createStatement();
+                ResultSet data = stmt.executeQuery(query);
+                while (data.next()) {
+                    System.out.format("%d\t%s\t%s\t%s\t%s\n\n\n\n",
+                            data.getInt(1),
+                            data.getString(2),
+                            data.getString(3),
+                            data.getString(4),
+                            data.getString(5));
+
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public void addBook(Book book){
+        String query = "INSERT INTO book(title,authorId,isbn,status) values(?,?,?,?)" ;
+        try {
+            PreparedStatement pstm = connection.prepareStatement(query);
+            pstm.setString(1, book.getTitle());
+            pstm.setInt(2, book.getAuthor());
+            pstm.setInt(3, book.getIsbn());
+            pstm.setString(4, book.getStatus());
+
+            int count = pstm.executeUpdate();
+            if (count != 0)
+                System.out.println("Book Inserted successfully.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
     public boolean findByIsbn(int isbn) throws SQLException {
         if (this.connection != null) {
             String query = "SELECT * FROM book WHERE isbn = ?";
@@ -24,6 +62,36 @@ public class BookService {
             }
         }
         return true;
+    }
+
+    public void searchForBook(String title){
+
+        if (connection != null) {
+            String query = "select * from book where book.title LIKE ?";
+            try {
+                PreparedStatement pstm = connection.prepareStatement(query);
+                pstm.setString(1, "%"+title+"%");
+
+                ResultSet result = pstm.executeQuery();
+                if (!result.isBeforeFirst()) {
+                    System.out.println("No book found with this word : " + title + "!");
+                }else {
+                    System.out.println("Result : ");
+                    System.out.format("%s\t%s\t%s\t%s\t%s\n", "Id", "Title", "Status", "Author","Isbn");
+                    while (result.next()){
+                        System.out.format("%d\t%s\t\t%s\t%s\t%s\n\n\n\n",
+                                result.getInt(1),
+                                result.getString(2),
+                                result.getString(3),
+                                result.getString(4),
+                                result.getString(5));
+
+                    }
+                }
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     public void upDateBook(Book book, int isbn) {
@@ -49,6 +117,19 @@ public class BookService {
     }
 
     public void delete(int isbn){
+        String query = "DELETE FROM book WHERE `isbn`=? ";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, isbn);
 
+            int count = preparedStatement.executeUpdate();
+            if (count != 0) {
+                System.out.println("Book deleted Successfully");
+            } else {
+                System.out.println("Something went wrong");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
