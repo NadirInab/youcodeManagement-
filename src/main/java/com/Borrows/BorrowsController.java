1e40ffe73c7c1e2b41206a00c028d76abf61313f
nadir inab <nadir.inab.dev@gmail.com>
2023-09-11 12:09:36 +0100

    Borrow Service and controller are well added

--- src/main/java/com/Borrows/BorrowsController.java
+++ src/main/java/com/Borrows/BorrowsController.java
@@ -1,5 +1,31 @@
 package main.java.com.Borrows;
 
+import main.java.com.DB.DatabaseConnection;
+
+import java.sql.Connection;
+
 public class BorrowsController {
 
+    Connection connection ;
+    BorrowsService borrowsService = new BorrowsService();
+
+    public BorrowsController() throws Exception{
+        DatabaseConnection databaseConnection = new DatabaseConnection() ;
+        this.connection = DatabaseConnection.getConnection() ;
+    }
+
+    public void index(){
+        this.borrowsService.index();
+    }
+    public void addBorrow(){
+        this.borrowsService.add();
+    }
+
+    public void delete(){
+       this.borrowsService.delete() ;
+    }
+
+    public void upDate(){
+        this.borrowsService.upDate();
+    }
 }
--- src/main/java/com/Borrows/BorrowsService.java
+++ src/main/java/com/Borrows/BorrowsService.java
@@ -1,4 +1,20 @@
 package main.java.com.Borrows;
 
 public class BorrowsService {
+
+    public void add(){
+        System.out.println("add service");
+    }
+
+    public void index(){
+        System.out.println("add service");
+    }
+
+    public void upDate(){
+        System.out.println("add service");
+    }
+
+    public void delete(){
+        System.out.println("add service");
+    }
 }
